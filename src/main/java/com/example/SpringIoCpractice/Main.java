8aa562fcb94fb2a5a8062cc73720eaf4e371aaba
juslly <30677857+juslly@users.noreply.github.com>
2019-08-08 20:01:06 +0800

    Update Main.java

--- src/main/java/com/example/SpringIoCpractice/Main.java
+++ src/main/java/com/example/SpringIoCpractice/Main.java
@@ -1,1 +1,20 @@
 //TODO
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+ 
+public class Main {
+ 
+    public static void main(String[] args) {
+        /*//创建HelloWorld的一个对象
+        HelloWorld helloWorld = new HelloWorld();
+        //为name属性赋值
+        helloWorld.setUserName("Tom");
+        */
+        //1、创建Spring的IOC容器
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+        //2、从IOC容器中获取Bean
+        Person person = (Person) ctx.getBean("Person");
+        //3、调用hello方法
+        person.name();
+    }
+}
