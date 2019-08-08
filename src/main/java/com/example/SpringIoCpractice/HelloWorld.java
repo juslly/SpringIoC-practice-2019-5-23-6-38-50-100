afb56886a8c1529391de02d723a4c87bbdcbee88
juslly <30677857+juslly@users.noreply.github.com>
2019-08-08 19:58:17 +0800

    Update HelloWorld.java

--- src/main/java/com/example/SpringIoCpractice/HelloWorld.java
+++ src/main/java/com/example/SpringIoCpractice/HelloWorld.java
@@ -2,6 +2,14 @@ package com.example.SpringIoCpractice;
 
 public class HelloWorld {
   private String hello;
+  
+  private String name;
+ 
+    public void setname(String name) {
+        System.out.println("SetName: " + name);
+        this.name = name;
+    }
+
 
   public void setHello(String hello) {
     this.hello = hello;
